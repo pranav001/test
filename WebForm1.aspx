391cbd0552f381b786eccb435b9ef1127619d14b
unknown <pranav.jha75@gmail.com>
2013-01-25 16:42:29 +0530

    files added

---
+++ WebForm1.aspx
@@ -0,0 +1,72 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="ffweb.WebForm1" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <asp:Repeater ID="Repeater1"  runat="server"  
+            onitemdatabound="Repeater1_ItemDataBound" 
+            onitemcommand="Repeater1_ItemCommand1"  >
+        
+        <HeaderTemplate>
+        <table>
+        <tr>
+         <th>
+        <asp:Label ID="Label3" runat="server" Text="EMail"></asp:Label></th>
+        
+       <th>
+        <asp:Label ID="Label1" runat="server" Text="FirstName"></asp:Label></th>
+
+        <th><asp:Label ID="Label2" runat="server" Text="LastName"></asp:Label></th>
+        <th>       
+        <asp:Label ID="Label4" runat="server" Text="Address"></asp:Label></th>
+        <th>
+        <asp:Label ID="Label5" runat="server" Text="Age"></asp:Label></th>
+        </tr>
+        
+        </HeaderTemplate>
+        <ItemTemplate>
+        <tr>       
+        <td>
+            <asp:TextBox ID="EmailTextBox" runat="server" Text='<%#Eval("email") %>'></asp:TextBox>
+        </td>
+      
+             <td>
+        <asp:TextBox ID="TextBox7" runat="server" Text='<%#Eval("firstname") %>'></asp:TextBox>
+        </td>
+      
+        <td>
+            <asp:TextBox ID="TextBox8" runat="server" Text='<%#Eval("lastname") %>'></asp:TextBox>
+        </td>
+        <td>
+            <asp:TextBox ID="TextBox9" runat="server" Text='<%#Eval("address") %>'></asp:TextBox>
+        </td>
+        <td>
+            <asp:TextBox ID="TextBox10" runat="server" Text='<%#Eval("age") %>'></asp:TextBox>
+        </td>
+
+          <td>
+            <asp:LinkButton ID="LinkButton1" CommandName="Delete" runat="server">Delete</asp:LinkButton>
+        </td>
+        
+          <td>
+            <asp:LinkButton ID="LinkButton2" CommandName="Edit" runat="server">Edit</asp:LinkButton>
+        </td>
+        </tr>
+       
+        </ItemTemplate>   
+        
+       
+        <FooterTemplate>
+        </table>
+        </FooterTemplate>     
+        </asp:Repeater>
+    </div>
+    </form>
+</body>
+</html>
---
+++ WebForm1.aspx.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+
+namespace ffweb
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        SqlConnection con = new SqlConnection(@"Data Source=RAJESH-PC\MSSQLSERVERR2;Initial Catalog=master;Integrated Security=True");
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+            show();
+        }
+        public void show()
+        {
+            con.Open();
+            SqlCommand cmd = new SqlCommand("select *from pranav", con);
+            SqlDataReader sdr = cmd.ExecuteReader();
+            Repeater1.DataSource = sdr;
+            Repeater1.DataBind();
+            con.Close();
+        }
+
+        protected void Repeater1_ItemDataBound(object sender, RepeaterItemEventArgs e)
+        {
+
+        }
+
+        protected void LinkButton1_btnclick(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Repeater1_ItemCommand(object source, RepeaterCommandEventArgs e)
+        {
+
+        }
+
+        protected void Repeater1_ItemCommand1(object source, RepeaterCommandEventArgs e)
+        {
+            if (e.CommandName == "Delete")
+            {
+                TextBox Age = (TextBox)e.Item.FindControl("TextBox10");
+                con.Open();
+
+                SqlCommand cmd = new SqlCommand("delete from pranav where age=" +  Age.Text + "", con);
+                int res=cmd.ExecuteNonQuery();
+                if (res > 0)
+                {
+                    Response.Write("Deleted");
+                }
+                con.Close();
+                show();
+                //}
+                //catch (Exception ex)
+                //{
+                //    Response.Write(ex.Message);
+                //}
+            }
+            if (e.CommandName == "Edit")
+            {
+                TextBox Email = (TextBox)e.Item.FindControl("TextBox6");
+                TextBox First = (TextBox)e.Item.FindControl("TextBox7");
+                TextBox Last = (TextBox)e.Item.FindControl("TextBox8");
+                TextBox Add = (TextBox)e.Item.FindControl("TextBox9");
+                TextBox Age = (TextBox)e.Item.FindControl("TextBox10");
+
+                con.Open();
+                SqlCommand cmd = new SqlCommand("update pranav set email='"+Email.Text+"', firstName='"+First.Text+"', lastName='"+Last.Text+"', address='"+Add.Text+"' where Age='"+Age.Text+"'",con);
+                cmd.ExecuteNonQuery();
+                con.Close();
+                show();
+                    
+            }
+
+        }
+
+    }
+}
